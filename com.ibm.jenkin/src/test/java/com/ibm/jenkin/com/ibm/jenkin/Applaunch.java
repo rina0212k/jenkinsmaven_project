b51b1327ab9f7d491576042d47847f7c57770143
RinaKagdelwar <RinaKagdelwar@9.81.222.191>
2019-01-04 16:42:53 +0530

    Test pass

---
+++ com.ibm.jenkin/src/test/java/com/ibm/jenkin/com/ibm/jenkin/Applaunch.java
@@ -0,0 +1,35 @@
+package com.ibm.jenkin.com.ibm.jenkin;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+
+public class Applaunch{
+	public WebDriver driver;
+	
+  @Test
+  public void f() {
+	  System.out.println("Browser is launched");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver","C:\\seleniumsetupfiles\\chromedriver_win32\\chromedriver.exe");
+	  driver = new ChromeDriver();
+	  driver.get("http://retail.hommelle.com/");
+	  String s = driver.getTitle();
+	  String expected = "Retail";
+	  assertEquals(s,expected);
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
